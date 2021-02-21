322080e5e366562e1ab733abbd97d8376ded9bc1
Bug <khanhnd.B18CN309@stu.ptit.edu.vn>
2021-02-21 21:21:43 +0700

    update search

--- routes/ProductType.js
+++ routes/ProductType.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const { getFullProductType, postProductType, postFullProductType } = require('../controllers/ProductType.js')
 
-router.get('/', getFullProductType);
-router.post('/', postProductType);
-router.post('/full', postFullProductType );
+router.get('/type', getFullProductType);
+router.post('/type', postProductType);
+router.post('/type/full', postFullProductType );
 module.exports = router;
